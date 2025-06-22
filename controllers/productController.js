9fede8c73ec21bfc6e00a95cf41cd18da59eb2a8
gthilakshana <gavrawavanniarachchi@gmail.com>
2025-06-22 04:53:34 -0700

    Added productController and productRouter

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,34 @@
 import Product from "../models/product.js";
+import { isAdmin } from "./userController.js";
 
 
 export function createProduct(req,res){
+    if(!isAdmin(req)){
+        res.json({
+            message: "Please login as administrator to create product",
+          });
+          return;
+    }
+
+    const newProductData = req.body;
+    const product = new Product(newProductData);
+
+    product.save()
+    .then(()=>{
+        res.json({
+            message: "Product saved successfully",
+          });
+    }).catch((error)=>{
+        res.json({
+            message: error
+          });
+    })
     
 }
+
+export function getProducts(req,res){
+    Product.find().then((product)=>{
+        res.json(product)
+    })
+}
+
