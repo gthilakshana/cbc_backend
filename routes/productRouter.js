b346871f85168d5ccd0dbbf279f3bc8411f0efb8
gthilakshana <gavrawavanniarachchi@gmail.com>
2025-06-27 13:01:45 -0700

    update product part completed 🐳

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createProduct, getProducts, deleteProduct } from "../controllers/productController.js";
+import { createProduct, getProducts, deleteProduct, updateProduct,getProductById } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/", createProduct);
 productRouter.get("/", getProducts);
 productRouter.delete("/:productId", deleteProduct);
+productRouter.put("/:productId", updateProduct);
+productRouter.get("/:productId", getProductById);
 
 export default productRouter;
