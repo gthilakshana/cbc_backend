fc6e989e1ba7535f61a172c2935a368991331762
gthilakshana <gavrawavanniarachchi@gmail.com>
2025-06-30 12:02:47 -0700

    userRouter change 🐬

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app.use((req, res, next) => {
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
+
  
 app.listen(5000, () => { 
   console.log("Listening on port 5000");
